71cbca224537f8a33b88e7dc30fb9fbff1db154b
nixzoid <nixzoid@jetpure.org>
2025-02-16 22:08:05 +0900

    shizofrenik ./pkgs, frfr i not joking

--- pkgs/README.md
+++
@@ -1,6 +0,0 @@
-> mostly i create pkgs via [nix-init](https://github.com/nix-community/nix-init)
-
-- list of pkgs:
-
-  1.  [./anicli](https://github.com/vypivshiy/ani-cli-ru)
-  2.  [./lowfi](https://github.com/talwat/lowfi)
--- pkgs/flake.nix
+++ pkgs/flake.nix
@@ -1,19 +1,4 @@
 {
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-
-  outputs =
-    inputs:
-    import ./outputs.nix {
-      inherit inputs;
-      list = [
-        "qTransPop"
-        "qTransPag"
-        "base16-preview"
-        "torrserver"
-        "anicliru"
-        "canvas"
-        "torque"
-        "lowfi"
-      ];
-    };
+  outputs = args: import ./outputs.nix args;
+  inputs.n.url = "github:NixOS/nixpkgs/nixos-unstable";
 }

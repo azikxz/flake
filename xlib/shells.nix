e454ef8ef9790f994e2abd36d6f7df342b737d77
nixzoid <nixzoid@jetpure.org>
2025-05-03 16:29:23 +0900

    devshells: rm alloy

--- xlib/shells.nix
+++ xlib/shells.nix
@@ -27,29 +27,4 @@ in
     python3
     pipx
   ];
-  alloy =
-    let
-      alloy6' = pkgs.stdenv.mkDerivation rec {
-        pname = "alloy${lib.versions.major version}";
-        version = "6.2.0";
-
-        src = pkgs.fetchurl {
-          sha256 = "sha256-a4wctbyTvt/HxhQ1xOGrbmiKJC3HAqOUYo2amAHtt40=";
-          url = "https://github.com/AlloyTools/org.alloytools.alloy/releases/download/v${version}/org.alloytools.alloy.dist.jar";
-        };
-
-        nativeBuildInputs = [ pkgs.makeWrapper ];
-        buildCommand = ''
-          jar=$out/share/alloy/${pname}.jar
-          install -Dm644 ${src} $jar
-
-          mkdir -p $out/bin
-          makeWrapper ${pkgs.jre}/bin/java $out/bin/${pname} --add-flags \
-            "-jar $jar"
-        '';
-      };
-    in
-    mk [
-      alloy6'
-    ];
 }

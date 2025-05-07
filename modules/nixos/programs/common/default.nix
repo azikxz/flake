366c9c3cffb72e0c4dc77dc112b99116716778b9
nixzoid <nixzoid@jetpure.org>
2025-05-07 20:03:16 +0900

    commonNixos: add uutils and extraPackages option

--- modules/nixos/programs/common/default.nix
+++ modules/nixos/programs/common/default.nix
@@ -8,23 +8,28 @@
 with lib;
 with x;
 let
-  cfg = config.module.programs.common;
+  cfg = config.module.programs;
 in
 
 {
   options = {
-    module.programs.common = {
-      enable = mkBool false;
+    module.programs = {
+      common.enable = mkBool false;
+      extraPackages = mkList.pkgs [ ];
     };
   };
 
-  config = mkIf cfg.enable {
-    environment.systemPackages = with pkgs; [
-      corefonts
-      killall
-      helix
-      fff
-    ];
+  config = mkIf cfg.common.enable {
+    environment.systemPackages =
+      with pkgs;
+      [
+        uutils-coreutils-noprefix
+        corefonts
+        killall
+        helix
+        fff
+      ]
+      + cfg.extraPackages;
     programs = {
       adb = if (sys.is == "laptop") then on else off;
       light = if (sys.is == "desktop") then on else off;

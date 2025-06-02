36f29d9e713476cbbddd00b695993fdc8b9ab5ee
nixzoid <nixzoid@jetpure.org>
2025-06-02 19:00:03 +0900

    anicliru: fix

--- modules/programs/cli/anicliru/default.nix
+++ modules/programs/cli/anicliru/default.nix
@@ -12,11 +12,13 @@ mkIf (itIs == "desktop" || itIs == "laptop") {
 
     shellAliases.anicli = # create ./headers.txt and https://github.com/vypivshiy/anicli-api/issues/42#issuecomment-2889017544
       "anicli-ru -q 1080 -s anilibme "
-      + (
-        if (pathExists ./headers.txt) then
-          (toString (pkgs.writeText "headers.txt" (readFile ./headers.txt)))
-        else
-          ""
-      );
+      + "--header-file ${
+        (
+          if (pathExists ./headers.txt) then
+            (toString (pkgs.writeText "headers.txt" (readFile ./headers.txt)))
+          else
+            ""
+        )
+      }";
   };
 }

92d9f79150dbc8169dbe7623fba32e5ce618e030
nixzoid <nixzoid@jetpure.org>
2025-06-02 17:32:33 +0900

    anicliru: add autorization

--- modules/programs/cli/anicliru/default.nix
+++ modules/programs/cli/anicliru/default.nix
@@ -7,5 +7,16 @@
 with lib;
 
 mkIf (itIs == "desktop" || itIs == "laptop") {
-  hm.home.packages = [ pkgs.anicliru ];
+  hm.home = {
+    packages = [ pkgs.anicliru ];
+
+    shellAliases.anicli = # create ./headers.txt and https://github.com/vypivshiy/anicli-api/issues/42#issuecomment-2889017544
+      "anicli-ru -q 1080 -s anilibme "
+      + (
+        if (pathExists ./headers.txt) then
+          (toString (pkgs.writeText "headers.txt" (readFile ./headers.txt)))
+        else
+          ""
+      );
+  };
 }

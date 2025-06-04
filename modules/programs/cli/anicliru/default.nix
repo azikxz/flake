638331c0d088486ec33db523b0dd59ea6fdedb96
nixzoid <nixzoid@jetpure.org>
2025-06-04 10:14:36 +0900

    anicliru: alias => pkg

--- modules/programs/cli/anicliru/default.nix
+++ modules/programs/cli/anicliru/default.nix
@@ -7,18 +7,17 @@
 with lib;
 
 mkIf (itIs == "desktop" || itIs == "laptop") {
-  hm.home = {
-    packages = [ pkgs.anicliru ];
-
-    shellAliases.anicli = # create ./headers.txt and https://github.com/vypivshiy/anicli-api/issues/42#issuecomment-2889017544
-      "anicli-ru -q 1080 -s anilibme "
-      + "--header-file ${
+  hm.home.packages =
+    with pkgs;
+    [ anicliru ]
+    ++ [
+      (writeScriptBin "anicli" ''anicli-ru -q 1080 -s anilibme ${
         (
           if (pathExists ./headers.txt) then
-            (toString (pkgs.writeText "headers.txt" (readFile ./headers.txt)))
+            "--header-file ${toString (writeText "headers.txt" (readFile ./headers.txt))}"
           else
             ""
         )
-      }";
-  };
+      }'')
+    ];
 }

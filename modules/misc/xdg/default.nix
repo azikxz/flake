4c592f2bc6c5180fb584a4ebbb2596f50ac2043f
nixzoid <nixzoid@jetpure.org>
2025-06-04 21:37:02 +0900

    xdg: remove .desktop from applications

--- modules/misc/xdg/default.nix
+++ modules/misc/xdg/default.nix
@@ -20,88 +20,99 @@ mkIf (itIs == "desktop" || itIs == "laptop") {
 
   environment.systemPackages = [ pkgs.xdg-user-dirs ];
 
-  hm.xdg = {
-    enable = true;
-
-    mime.enable = true;
-
-    mimeApps =
-      {
-        enable = true;
-      }
-      // import ./mimeApps.nix {
-        inherit
-          lib
-          ;
-      };
-
-    portal = {
+  hm = {
+    xdg = {
       enable = true;
 
-      config.common.default = [ "gtk" ];
+      mime.enable = true;
 
-      extraPortals = with pkgs; [
-        xdg-desktop-portal-gtk
-        xdg-desktop-portal-hyprland
-      ];
-    };
-
-    userDirs =
-      let
-        mk = config.hm.home.homeDirectory;
-      in
-      {
-        enable = true;
-      }
-      // rec {
-        # xdg default
-        desktop = mk + "/Desktop";
-        documents = mk + "/Documents";
-        download = mk + "/Downloads";
-        music = mk + "/Music";
-        pictures = mk + "/Pictures";
-        publicShare = mk + "/";
-        templates = mk + "/";
-        videos = mk + "/Videos";
-
-        createDirectories = true;
-
-        extraConfig = {
-          XDG_STUDY_DIR = mk + "/Study";
-          XDG_SCREENSHOTS_DIR = pictures + "/Screenshots";
-          XDG_FLAKE_DIR = paths.flakeDir;
-          XDG_TORRENTS_DIR = config.services.qbittorrent.savePath;
+      mimeApps =
+        {
+          enable = true;
+        }
+        // import ./mimeApps.nix {
+          inherit
+            lib
+            ;
         };
+
+      portal = {
+        enable = true;
+
+        config.common.default = [ "gtk" ];
+
+        extraPortals = with pkgs; [
+          xdg-desktop-portal-gtk
+          xdg-desktop-portal-hyprland
+        ];
       };
 
-    desktopEntries =
-      let
-        mk = name: {
-          inherit name;
-          noDisplay = true;
+      userDirs =
+        let
+          mk = config.hm.home.homeDirectory;
+        in
+        {
+          enable = true;
+        }
+        // rec {
+          # xdg default
+          desktop = mk + "/Desktop";
+          documents = mk + "/Documents";
+          download = mk + "/Downloads";
+          music = mk + "/Music";
+          pictures = mk + "/Pictures";
+          publicShare = mk + "/";
+          templates = mk + "/";
+          videos = mk + "/Videos";
+
+          createDirectories = true;
+
+          extraConfig = {
+            XDG_STUDY_DIR = mk + "/Study";
+            XDG_SCREENSHOTS_DIR = pictures + "/Screenshots";
+            XDG_FLAKE_DIR = paths.flakeDir;
+            XDG_TORRENTS_DIR = config.services.qbittorrent.savePath;
+          };
         };
+
+      desktopEntries =
+        let
+          mk = name: {
+            inherit name;
+            noDisplay = true;
+          };
+        in
+        {
+          # qt
+          qt5ct = mk "qt5ct";
+          qt6ct = mk "qt6ct";
+
+          # cli
+          nvtop = mk "nvtop";
+          btop = mk "btop";
+          fish = mk "fish";
+
+          # shit
+          rofi = mk "rofi";
+          rofi-theme-selector = mk "rofi-theme-selector";
+
+          # office
+          base = mk "base";
+          calc = mk "calc";
+          draw = mk "draw";
+          impress = mk "impress";
+          math = mk "math";
+          writer = mk "writer";
+        };
+    };
+    home.activation =
+      let
+        applications = "${config.hm.xdg.dataHome}/applications";
       in
       {
-        # qt
-        qt5ct = mk "qt5ct";
-        qt6ct = mk "qt6ct";
-
-        # cli
-        nvtop = mk "nvtop";
-        btop = mk "btop";
-        fish = mk "fish";
-
-        # shit
-        rofi = mk "rofi";
-        rofi-theme-selector = mk "rofi-theme-selector";
-
-        # office
-        base = mk "base";
-        calc = mk "calc";
-        draw = mk "draw";
-        impress = mk "impress";
-        math = mk "math";
-        writer = mk "writer";
+        rmDesktop = hm.dag.entryAfter [ "" ] ''
+          run rm ${applications}/*.desktop && rm ${applications}/wine
+        '';
       };
   };
 }

98fb0e93189ddb0c9b9e4f3d5ce6ff447a9963ae
nixzoid <nixzoid@jetpure.org>
2025-06-15 14:10:34 +0900

    nh: change aliases

--- modules/programs/cli/nixHelper/default.nix
+++ modules/programs/cli/nixHelper/default.nix
@@ -6,14 +6,10 @@
 with lib;
 
 mkIf (itIs == "desktop" || itIs == "laptop") {
-  environment = {
-    variables.NH_FLAKE = paths.flakeDir;
-
-    shellAliases = {
-      goToFlake = "cd ${paths.flakeDir}";
-      rebuild = "nh os switch -H ${machineName}";
-      nixRepl = "nh os repl -H ${machineName}";
-    };
+  environment.shellAliases = {
+    goToFlake = "cd ${paths.flakeDir}";
+    rebuild = "nh os switch -H ${machineName}";
+    nixRepl = "nixos-rebuild repl --flake ${paths.flakeDir}#${machineName}";
   };
 
   programs.nh = {

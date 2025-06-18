54e9bcbc7ab7b11a307d644347ce0574e85b6a81
nixzoid <nixzoid@jetpure.org>
2025-06-18 17:09:09 +0300

    davinci: update

--- modules/programs/gui/davinci/default.nix
+++ modules/programs/gui/davinci/default.nix
@@ -6,67 +6,59 @@
 
 with lib;
 let
-  isStudio = false;
+  fix = ''s/\x74\x11\xe8\x21\x23\x00\x00/\xeb\x11\xe8\x21\x23\x00\x00/g'';
 in
 
 mkIf false {
-  environment.systemPackages =
-    with pkgs;
-    [ ]
-    ++ (
-      if (isStudio == true) then
-        [
-          (davinci-resolve-studio.override (old: {
-            buildFHSEnv =
-              fhs:
-              (
-                let
-                  davinci = fhs.passthru.davinci.overrideAttrs (old: {
-                    postFixup = ''
-                      ${old.postFixup}
-                      ${getExe perl} -pi -e 's/\x74\x11\xe8\x21\x23\x00\x00/\xeb\x11\xe8\x21\x23\x00\x00/g' $out/bin/resolve
-                    '';
-                  });
-                in
-                old.buildFHSEnv (
-                  fhs
-                  // {
-                    extraBwrapArgs = [
-                      ''--bind "$HOME"/.local/share/DaVinciResolve/license ${davinci}/.license''
-                      # "--bind /run/opengl-driver/etc/OpenCL /etc/OpenCL"
-                    ];
+  environment.systemPackages = with pkgs; [
+    (davinci-resolve-studio.override (old: {
+      buildFHSEnv =
+        fhs:
+        (
+          let
+            davinci = fhs.passthru.davinci.overrideAttrs (old: {
+              postFixup = ''
+                ${old.postFixup}
+                ${getExe perl} -pi -e '${fix}' $out/bin/resolve
+              '';
+            });
+          in
+          old.buildFHSEnv (
+            fhs
+            // {
+              extraBwrapArgs = [
+                ''--bind "$HOME"/.local/share/DaVinciResolve/license ${davinci}/.license''
+                # "--bind /run/opengl-driver/etc/OpenCL /etc/OpenCL"
+              ];
 
-                    runScript = "${getExe bash} ${writeText "davinci-wrapper" ''
-                      export QT_XKB_CONFIG_ROOT="${xkeyboard_config}/share/X11/xkb"
+              runScript = "${getExe bash} ${writeText "davinci-wrapper" ''
+                export QT_XKB_CONFIG_ROOT="${xkeyboard_config}/share/X11/xkb"
 
-                      export QT_PLUGIN_PATH="${davinci}/libs/plugins:$QT_PLUGIN_PATH"
+                export QT_PLUGIN_PATH="${davinci}/libs/plugins:$QT_PLUGIN_PATH"
 
-                      export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib:/usr/lib32:${davinci}/libs
+                export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib:/usr/lib32:${davinci}/libs
 
-                      unset QT_QPA_PLATFORM # Having this set to wayland causes issues
+                unset QT_QPA_PLATFORM # Having this set to wayland causes issues
 
-                      ${davinci}/bin/resolve
-                    ''}";
+                ${davinci}/bin/resolve
+              ''}";
 
-                    extraInstallCommands = ''
-                      mkdir -p $out/share/applications $out/share/icons/hicolor/128x128/apps
+              extraInstallCommands = ''
+                mkdir -p $out/share/applications $out/share/icons/hicolor/128x128/apps
 
-                      ln -s ${davinci}/share/applications/*.desktop $out/share/applications/
+                ln -s ${davinci}/share/applications/*.desktop $out/share/applications/
 
-                      ln -s ${davinci}/graphics/DV_Resolve.png $out/share/icons/hicolor/128x128/apps/davinci-resolve-studio.png
-                    '';
+                ln -s ${davinci}/graphics/DV_Resolve.png $out/share/icons/hicolor/128x128/apps/davinci-resolve-studio.png
+              '';
 
-                    passthru = {
-                      inherit
-                        davinci
-                        ;
-                    };
-                  }
-                )
-              );
-          }))
-        ]
-      else
-        davinci-resolve
-    );
+              passthru = {
+                inherit
+                  davinci
+                  ;
+              };
+            }
+          )
+        );
+    }))
+  ];
 }

45eed2a0bfebd73bcd5f89d3c0cd55d20b27b881
nixzoid <nixzoid@jetpure.org>
2025-06-18 23:54:44 +0900

    minecraft: init config

--- modules/games/minecraft/default.nix
+++ modules/games/minecraft/default.nix
@@ -1,32 +1,61 @@
 {
   pkgs,
   lib,
+  config,
   ...
 }:
 
 with lib;
+let
+  imp = "gui";
+in
 
 mkIf (itIs == "desktop" || itIs == "laptop") {
-  persist.user.dirs = [
-    ".local/share/PrismLauncher"
-    ".config/ferium"
-    ".minecraft"
-  ];
+  persist.user.dirs =
+    if (imp == "cli") then
+      [
+        ".config/ferium"
+        ".minecraft"
+      ]
+    else
+      [
+        ".local/share/PrismLauncher"
+      ];
 
-  environment.systemPackages = with pkgs; [
-    (prismlauncher.override {
-      gamemodeSupport = true;
-      controllerSupport = true;
-      textToSpeechSupport = false;
+  environment.systemPackages =
+    with pkgs;
+    if (imp == "cli") then
+      [
+        ferium
+        portablemc
+      ]
+    else
+      [
+        (prismlauncher.override {
+          gamemodeSupport = true;
+          controllerSupport = true;
+          textToSpeechSupport = false;
 
-      jdks = builtins.attrValues {
-        inherit
-          temurin-jre-bin-23
-          temurin-jre-bin # 21
-          temurin-jre-bin-17
-          temurin-jre-bin-8
-          ;
-      };
-    })
-  ];
+          jdks = builtins.attrValues {
+            inherit
+              temurin-jre-bin-23
+              temurin-jre-bin # 21
+              temurin-jre-bin-17
+              temurin-jre-bin-8
+              ;
+          };
+        })
+      ];
+
+  hm.xdg.dataFile."PrismLauncher/prismlauncher.cfg".source =
+    (pkgs.formats.ini { }).generate "prismlauncher-settings"
+      (
+        import ./settings.nix {
+          inherit
+            pkgs
+            lib
+            config
+            ;
+        }
+      );
 }
---
+++ modules/games/minecraft/settings.nix
@@ -0,0 +1,68 @@
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+
+with lib;
+
+{
+  General = {
+    ApplicationTheme = "system";
+    AutoCloseConsole = false;
+    AutomaticJavaDownload = false;
+    AutomaticJavaSwitch = false;
+    CatOpacity = 100;
+    CentralModsDir = "mods";
+    CloseAfterLaunch = false;
+    ConfigVersion = "1.2";
+    ConsoleFont = config.stylix.fonts.monospace.name;
+    ConsoleOverflowStop = true;
+    DownloadsDir = "/home/nixzoid/Downloads";
+    DownloadsDirWatchRecursive = false;
+    EnableFeralGamemode = true;
+    EnableMangoHud = config.hm.programs.mangohud.enable;
+    IconTheme = "pe_light";
+    IconsDir = "icons";
+    IgnoreJavaCompatibility = false;
+    IgnoreJavaWizard = true;
+    InstSortMode = "Name";
+    InstanceDir = "instances";
+    JavaDir = "java";
+    JavaPath = getExe pkgs.temurin-jre-bin;
+    Language = "en_US";
+    LastHostname = "jetpure";
+    LaunchMaximized = false;
+    MaxMemAlloc = if (itIs == "desktop") then 16384 else 8192;
+    MenuBarInsteadOfToolBar = true;
+    MinMemAlloc = 512;
+    ModDependenciesDisabled = false;
+    ModMetadataDisabled = false;
+    NumberOfConcurrentDownloads = 6;
+    NumberOfConcurrentTasks = 10;
+    NumberOfManualRetries = 1;
+    OnlineFixes = true;
+    PastebinType = 3;
+    PermGen = 128;
+    QuitAfterGameStop = false;
+    RecordGameTime = true;
+    RequestTimeout = 60;
+    ShowConsole = false;
+    ShowConsoleOnError = true;
+    ShowGameTime = true;
+    ShowGameTimeWithoutDays = false;
+    ShowGlobalGameTime = true;
+    SkinsDir = "skins";
+    SkipModpackUpdatePrompt = false;
+    StatusBarVisible = false;
+    TechnicClientID = "";
+    ToolbarsLocked = true;
+    UseDiscreteGpu = false;
+    UseNativeGLFW = false;
+    UseNativeOpenAL = false;
+    UseZink = false;
+    UserAgentOverride = "";
+    UserAskedAboutAutomaticJavaDownload = true;
+  };
+}

99c492fee98437da036b85960eca851a120c74d0
nixzoid <nixzoid@jetpure.org>
2025-06-19 15:06:54 +0900

    mangohud: init

---
+++ modules/games/mangohud/default.nix
@@ -0,0 +1,22 @@
+{
+  lib,
+  ...
+}:
+
+with lib;
+
+mkIf (itIs == "desktop") {
+  persist.user.dirs = [ ".mangohud-logs" ];
+
+  hm.programs.mangohud = {
+    enable = true;
+    enableSessionWide = true;
+
+    settings = {
+      preset = 0;
+
+      log_interval = 500;
+      output_folder = "$HOME/.mangohud-logs";
+    };
+  };
+}

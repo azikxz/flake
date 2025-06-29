a9827b5d2678d45184a65cc18079d6322cbbc64e
nixzoid <nixzoid@jetpure.org>
2025-06-29 13:51:51 +0900

    stylix: image refactor

--- modules/themes/stylix/image.nix
+++ modules/themes/stylix/image.nix
@@ -6,27 +6,14 @@
   ...
 }:
 
-with config.lib.stylix.colors;
-
-pkgs.runCommand "output.png" { } ''
-  ${lib.getExe pkgs.lutgen} apply ${inputs.wallpapers."${lib.style.image}"} -o $out -- ${
-    builtins.concatStringsSep " " [
-      base00
-      base01
-      base02
-      base03
-      base04
-      base05
-      base06
-      base07
-      base08
-      base09
-      base0A
-      base0B
-      base0C
-      base0D
-      base0E
-      base0F
-    ]
-  }
-''
+pkgs.runCommand "output.png" { } (
+  lib.concatStringsSep " " [
+    (lib.getExe pkgs.lutgen)
+    "apply"
+    inputs.wallpapers.${lib.style.image}
+    "-o"
+    "$out"
+    "--"
+    (builtins.concatStringsSep " " config.lib.stylix.colors.toList)
+  ]
+)

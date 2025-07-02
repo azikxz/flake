e40161685cb1d379ef91878118f6f9352c881551
toneus <toneus@blackmamba.org>
2025-07-02 14:29:31 +0900

    keepass: add autostart

--- modules/programs/gui/keepAss/default.nix
+++ modules/programs/gui/keepAss/default.nix
@@ -1,5 +1,6 @@
 {
   lib,
+  config,
   ...
 }:
 
@@ -10,5 +11,19 @@ mkIf (machine == "pcRyazenka" || machine == "thinkpadT14") {
     enable = true;
 
     settings = import ./settings.nix;
+  };
+
+  systemd.user.services.keepassxc = {
+    description = "Autostart for keepassxc";
+
+    script = concatStringsSep " " [
+      (getExe config.hm.programs.keepassxc.package)
+      "--minimized"
+      "--pw-stdin"
+      "~/Documents/passwords/db.kdbx"
+    ];
+
+    wantedBy = [ "graphical-session.target" ];
+    partOf = [ "graphical-session.target" ];
   };
 }

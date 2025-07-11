b7d4f5216809c4a712761185b1d1811e49fe4061
nixzoid <nixzoid@jetpure.org>
2025-07-11 20:28:46 +0900

    packages: init protons

---
+++ packages/proton/em.nix
@@ -0,0 +1,13 @@
+{ pkgs }:
+
+(pkgs.proton-ge-bin.overrideAttrs rec {
+  pname = "proton-em";
+  version = "10.0-24";
+
+  src = pkgs.fetchzip {
+    url = "https://github.com/Etaash-mathamsetty/Proton/releases/download/EM-${version}/proton-EM-${version}.tar.xz";
+    sha256 = "sha256-fgE/J88LzIE/nLTcpaiUAvBvwtAe+m1E0JW+Eq9W2a0=";
+  };
+
+}).override
+  { steamDisplayName = "Proton-EM"; }

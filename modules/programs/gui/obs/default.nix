51243b393683b07141c381bc1c7872d95b83a3d6
nixzoid <nixzoid@jetpure.org>
2025-07-13 14:09:17 +0900

    obs-studio: add theme

--- modules/programs/gui/obs/default.nix
+++ modules/programs/gui/obs/default.nix
@@ -1,23 +1,36 @@
 {
   pkgs,
   lib,
+  config,
   ...
 }:
 
 with lib;
 
 mkIf (mac "pcRyazenka") {
-  hm.programs.obs-studio = {
-    enable = true;
+  hm = {
+    programs.obs-studio = {
+      enable = true;
 
-    plugins = with pkgs.obs-studio-plugins; [
-      input-overlay
-      obs-backgroundremoval
-      obs-multi-rtmp
-      obs-pipewire-audio-capture
-      obs-vaapi
-      obs-vkcapture
-      wlrobs
-    ];
+      plugins = with pkgs.obs-studio-plugins; [
+        input-overlay
+        obs-backgroundremoval
+        obs-multi-rtmp
+        obs-pipewire-audio-capture
+        obs-vaapi
+        obs-vkcapture
+        wlrobs
+      ];
+    };
+
+    xdg.configFile = {
+      "obs-studio/themes/stylix.ovt".text = import ./ovt.nix {
+        inherit
+          config
+          ;
+      };
+
+      "obs-studio/themes/stylix.obt".text = import ./obt.nix;
+    };
   };
 }

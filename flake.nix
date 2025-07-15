9d88c515379d597154a9513f1da8b9e57c83c2cd
nixzoid <nixzoid@jetpure.org>
2025-07-15 22:28:29 +0900

    flake: pretty

--- flake.nix
+++ flake.nix
@@ -11,7 +11,18 @@
     hardware.url = "github:NixOS/nixos-hardware/master";
     flake-utils.url = "github:numtide/flake-utils";
     flatpak.url = "github:gmodena/nix-flatpak";
-    stylix.url = "github:danth/stylix";
+
+    parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+
+    stylix = {
+      url = "github:danth/stylix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-parts.follows = "parts";
+      inputs.nur.follows = "nur";
+    };
 
     home = {
       url = "github:nix-community/home-manager";
@@ -21,11 +32,13 @@
     nur = {
       url = "github:nix-community/NUR";
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-parts.follows = "parts";
     };
 
     gaming = {
       url = "github:fufexan/nix-gaming";
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-parts.follows = "parts";
     };
 
     disko = {
@@ -35,10 +48,8 @@
 
     niri = {
       url = "github:sodiboo/niri-flake";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        nixpkgs-stable.follows = "nixpkgs-24";
-      };
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs-stable.follows = "nixpkgs-24";
     };
 
     spicetify = {
@@ -49,6 +60,7 @@
     nixcord = {
       url = "github:kaylorben/nixcord";
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-parts.follows = "parts";
     };
 
     fjordlauncher = {
@@ -61,11 +73,6 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    libvirt = {
-      url = "github:AshleyYakeley/NixVirt";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
     winapps = {
       url = "github:winapps-org/winapps";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -74,11 +81,9 @@
     # zapret
     zapret = {
       url = "github:kotudemo/zapret-presets";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        secret_files.follows = "secret_files";
-        zapret-hostlists.follows = "zapret-hostlists";
-      };
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.secret_files.follows = "secret_files";
+      inputs.zapret-hostlists.follows = "zapret-hostlists";
     };
 
     secret_files = {
@@ -94,26 +99,20 @@
     # selfmade
     nixages = {
       url = "sourcehut:~neverness/nixages";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        flake-utils.follows = "flake-utils";
-      };
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
     };
 
     torrHelper = {
       url = "github:mctrxnv/torrserver-helper";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        flake-utils.follows = "flake-utils";
-      };
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
     };
 
     qbHelper = {
       url = "github:mctrxnv/qbHelper";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        flake-utils.follows = "flake-utils";
-      };
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
     };
 
     # selfmade style
@@ -132,6 +131,7 @@
       url = "github:Open-Wine-Components/umu-protonfixes";
       flake = false;
     };
+
     umu-database = {
       url = "https://raw.githubusercontent.com/Open-Wine-Components/umu-database/refs/heads/main/umu-database.csv";
       flake = false;

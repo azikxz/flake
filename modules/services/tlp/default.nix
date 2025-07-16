c649a59a64ae02abd3d095a4df7a5d164b88d5c5
toneus <toneus@blackmamba.org>
2025-07-16 14:26:46 +0900

    tlp: change mod

--- modules/services/tlp/default.nix
+++ modules/services/tlp/default.nix
@@ -21,10 +21,10 @@ mkIf (mac "thinkpadT14") {
       MEM_SLEEP_ON_BAT = "deep";
 
       # cpu
-      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_AC = "powersave";
       CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
 
-      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+      CPU_ENERGY_PERF_POLICY_ON_AC = "balance_performance";
       CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_performance";
 
       CPU_BOOST_ON_AC = 1;

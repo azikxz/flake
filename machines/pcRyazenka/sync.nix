94c7ca9b98afdf391b1b90a43398f63028c95029
nixzoid <nixzoid@jetpure.org>
2025-07-25 11:08:11 +0900

    syncthing: nothing2a change id

--- machines/pcRyazenka/sync.nix
+++ machines/pcRyazenka/sync.nix
@@ -7,7 +7,7 @@ with lib.sync;
 
 {
   hm.services.syncthing.settings.devices = builtins.listToAttrs [
-    (mkDevice "nothing2a" "HP32RHM-IQ6E35L-WBKIDIB-N5SXMMD-WPLNXRP-JQSWGZ4-YMRLJXW-Q6ZHUAZ")
+    (mkDevice "nothing2a" "NGCBPYO-2DZI7E7-7DW6S4K-7YFSU6Z-XD4ZDGS-L6XVCY7-PBB75OX-WLF36AF")
     (mkDevice "blackmamba" "NUJDVWO-GBUQV64-ULXJUIK-4JQ7J6N-T6GZ7H4-2G7BNMN-WBIY646-U5HCCAA")
   ];
 }
--- machines/thinkpadT14/sync.nix
+++ machines/thinkpadT14/sync.nix
@@ -7,7 +7,7 @@ with lib.sync;
 
 {
   hm.services.syncthing.settings.devices = builtins.listToAttrs [
-    (mkDevice "nothing2a" "HP32RHM-IQ6E35L-WBKIDIB-N5SXMMD-WPLNXRP-JQSWGZ4-YMRLJXW-Q6ZHUAZ")
+    (mkDevice "nothing2a" "NGCBPYO-2DZI7E7-7DW6S4K-7YFSU6Z-XD4ZDGS-L6XVCY7-PBB75OX-WLF36AF")
     (mkDevice "jetpure" "MXTZHNN-YURXR4T-OOMPUYJ-I6CSHN7-RPIQPRG-JMOKQGF-SYM4G7D-5QGBZAF")
   ];
 }

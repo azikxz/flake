879c7f0170f7fe7893d1ae3bb0db81b97d6557e9
nixzoid <nixzoid@jetpure.org>
2025-07-27 11:47:23 +0900

    gdu: fixes

--- modules/programs/cli/gdu/default.nix
+++ modules/programs/cli/gdu/default.nix
@@ -12,53 +12,47 @@ mkIf (mac "pcRyazenka" || mac "thinkpadT14") {
 
   hm.home.file.".gdu.yaml".text =
     with config.lib.stylix.colors.withHashtag;
-    (generators.toYAML { } {
-      style = {
-        log-file = "/dev/null";
+    # yaml
+    ''
+      style:
+        selected-row:
+          text-color: "${base0B}"
+          background-color: "${base02}"
+        footer:
+          text-color: "${base05}"
+          background-color: "${base01}"
+          number-color: "${base08}"
+        result-row:
+          directory-color: "${base0B}"
+          number-color: "${base0F}"
+        header:
+          hidden: true
 
-        max-cores = 4;
-        follow-symlinks = false;
+      log-file: "/dev/null"
+      max-cores: 4
+      follow-symlinks: false
 
-        show-apparent-size = true;
-        show-relative-size = false;
-        show-item-count = true;
-        show-mtime = true;
-        sequential-scanning = false;
+      show-apparent-size: true
+      show-relative-size: false
+      show-item-count: true
+      show-mtime: true
+      sequential-scanning: false
 
-        no-mouse = true;
-        non-interactive = false;
-        no-progress = false;
-        no-unicode = false;
-        no-cross = false;
-        no-hidden = false;
-        no-delete = true;
+      no-mouse: true
+      non-interactive: false
+      no-progress: false
+      no-unicode: false
+      no-cross: false
+      no-hidden: false
+      no-delete: true
 
-        use-si-prefix = true;
+      use-si-prefix: true
 
-        selected-row = {
-          text-color = base0B;
-          background-color = base02;
-        };
-
-        footer = {
-          text-color = base05;
-          background-color = base01;
-          number-color = base08;
-        };
-
-        result-row = {
-          directory-color = base0B;
-          number-color = base0F;
-        };
-
-        header = {
-          text-color = base05;
-          background-color = base01;
-          hidden = true;
-        };
-
-        progress-modal.current-item-path-max-len = 0;
-        style.use-old-size-bar = true;
-      };
-    });
+      progress-modal.current-item-path-max-len: 0
+      style.use-old-size-bar: true
+      delete-in-background: true
+      delete-in-parallel: true
+      sorting:
+        order: "desc"
+    '';
 }

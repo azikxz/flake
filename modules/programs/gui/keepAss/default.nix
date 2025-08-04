9910f435d964888dd6a3a0119baa4b96b19a4f8f
toneus <toneus@blackmamba.org>
2025-08-04 14:04:57 +0900

    keepassxc: added geometry in window

--- modules/programs/gui/keepAss/default.nix
+++ modules/programs/gui/keepAss/default.nix
@@ -9,10 +9,28 @@ with lib;
 # (only qutebrowser)
 
 mkIf (mac "pcRyazenka" || mac "thinkpadT14") {
-  hm.programs.keepassxc = {
-    enable = true;
+  hm = {
+    programs.keepassxc = {
+      enable = true;
 
-    settings = import ./settings.nix;
+      settings = import ./settings.nix;
+    };
+
+    xdg.cacheFile."keepassxc/keepassxc.ini".text =
+      # ini
+      ''
+        [General]
+        LastChallengeResponse=@Variant(\0\0\0\x1c\0\0\0\0)
+        LastDatabases=${config.hm.xdg.userDirs.documents}/passwords/db.kdbx
+        LastKeyFiles=@Variant(\0\0\0\x1c\0\0\0\0)
+
+        [GUI]
+        GroupSplitterState=0, 965
+        ListViewState=@ByteArray(\0\0\0\xff\0\0\0\0\0\0\0\x1\0\0\0\0\0\0\0\x1\x1\0\0\0\x11\0\0\0\0\0\0\0\x4\0\0\0\x5\0\0\0\x6\0\0\0\a\0\0\0\b\0\0\0\t\0\0\0\n\0\0\0\v\0\0\0\f\0\0\0\x1\0\0\0\r\0\0\0\x3\0\0\0\xe\0\0\0\x2\0\0\0\xf\0\0\0\x10\0\0\0\x11\0\0\0\0\0\0\0\n\0\0\0\xe\0\0\0\f\0\0\0\x1\0\0\0\x2\0\0\0\x3\0\0\0\x4\0\0\0\x5\0\0\0\x6\0\0\0\a\0\0\0\b\0\0\0\t\0\0\0\v\0\0\0\r\0\0\0\xf\0\0\0\x10\0\0\0\x11\x63\xf7\x1\0\0\0\f\0\0\0\v\0\0\0\x64\0\0\0\x5\0\0\x1M\0\0\0\x6\0\0\0\x64\0\0\0\a\0\0\0\x64\0\0\0\0\0\0\0\x64\0\0\0\x10\0\0\0\x64\0\0\0\x2\0\0\x1M\0\0\0\x3\0\0\0\x64\0\0\0\r\0\0\0\x64\0\0\0\xf\0\0\0\x1a\0\0\0\t\0\0\0\x64\0\0\0\n\0\0\0\x1a\0\0\x2\x65\0\0\0\x11\x1\x1\0\0\0\0\0\0\0\0\0\0\0\0\0\0\x64\0\0\0\x1\0\0\0\x81\0\0\0\0\0\0\0\x11\0\0\0\0\0\0\0\x1\0\0\0\0\0\0\0\0\0\0\0\x1\0\0\0\x2\0\0\0\x1a\0\0\0\x1\0\0\0\x2\0\0\0\x1a\0\0\0\x1\0\0\0\x2\0\0\0\x8c\0\0\0\x1\0\0\0\0\0\0\0\0\0\0\0\x1\0\0\0\0\0\0\0\0\0\0\0\x1\0\0\0\0\0\0\0\xf5\0\0\0\x1\0\0\0\0\0\0\0\0\0\0\0\x1\0\0\0\0\0\0\0\0\0\0\0\x1\0\0\0\0\0\0\0\0\0\0\0\x1\0\0\0\0\0\0\0\xb0\0\0\0\x1\0\0\0\0\0\0\0\0\0\0\0\x1\0\0\0\0\0\0\0\0\0\0\0\x1\0\0\0\0\0\0\0\0\0\0\0\x1\0\0\0\0\0\0\0\0\0\0\0\x1\0\0\0\0\0\0\0\0\0\0\0\x1\0\0\0\0\0\0\x3\xe8\x1\0\0\0\x64)
+        PreviewSplitterState=969, 0
+        SearchViewState=@ByteArray()
+        SplitterState=178, 1720
+      '';
   };
 
   systemd.user.services.keepassxc = {

6c7ff202bde209673d5e33ca8f927f7ee53d6e19
toneus <toneus@blackmamba.org>
2025-08-04 14:05:47 +0900

    obs -> recording: init wf-recorder

--- modules/programs/gui/recording/default.nix
+++ modules/programs/gui/recording/default.nix
@@ -7,8 +7,17 @@
 
 with lib;
 # INFO: set theme in settings
+# and wf-recorded
 
-mkIf (mac "pcRyazenka") {
+mkIf (mac "pcRyazenka" || mac "thinkpadT14") {
+  environment.systemPackages = with pkgs; [
+    wf-recorder
+    (writeShellScriptBin "wf-rec" ''
+      wf-recorder -r 60 -a \
+        -f "$HOME/Videos/Screencasts/$(date +'%Y%m%dT%H%M%S').mp4"
+    '')
+  ];
+
   hm = {
     programs.obs-studio = {
       enable = true;

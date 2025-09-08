908b85e91ceb3c25405d9c069c288dead8578726
toneus <toneus@blackmamba.org>
2025-09-08 11:10:48 +0900

    figlet: pretty

--- modules/programs/cli/figlet/default.nix
+++ modules/programs/cli/figlet/default.nix
@@ -11,16 +11,33 @@ mkIf false {
 
   hm.xdg.configFile =
     let
-      mk =
-        name: sha256:
-        pkgs.fetchurl {
-          url = "http://www.figlet.org/fonts/${name}.flf";
-          inherit sha256;
+      mkFont =
+        {
+          name,
+          sha256,
+        }:
+        {
+          name = "figlet/${name}.flf";
+          value.source = pkgs.fetchurl {
+            url = "http://www.figlet.org/fonts/${name}.flf";
+            inherit sha256;
+          };
         };
     in
-    {
-      "figlet/doom.flf".source = mk "doom" "sha256-PY/pljUmMp/51UFKl7D0fjqFLS8oimk/KThzD1ojDnU=";
-      "figlet/epic.flf".source = mk "epic" "sha256-fzh5bNy8dGtCumgEmnSxNZ+67ndr2Q/1saKoW3W/kiY=";
-      "figlet/small.flf".source = mk "small" "sha256-YybYpxjLV2idXqqY0wG5KX729jACeWNbkBUyovPWqt8=";
-    };
+    listToAttrs (
+      map mkFont [
+        {
+          name = "doom";
+          sha256 = "sha256-PY/pljUmMp/51UFKl7D0fjqFLS8oimk/KThzD1ojDnU=";
+        }
+        {
+          name = "epic";
+          sha256 = "sha256-fzh5bNy8dGtCumgEmnSxNZ+67ndr2Q/1saKoW3W/kiY=";
+        }
+        {
+          name = "small";
+          sha256 = "sha256-YybYpxjLV2idXqqY0wG5KX729jACeWNbkBUyovPWqt8=";
+        }
+      ]
+    );
 }

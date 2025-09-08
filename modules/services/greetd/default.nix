09b55b69894a573485eead8aea672b4302ab6383
toneus <toneus@blackmamba.org>
2025-09-08 11:49:46 +0900

    greetd: upstream

--- modules/services/greetd/default.nix
+++ modules/services/greetd/default.nix
@@ -19,35 +19,34 @@ mkIf (mac' "isoXtended") {
   services.greetd = {
     enable = true;
 
-    settings =
-      {
-        initial_session = mk (getExe config.programs.hyprland.package);
-      }
-      // {
-        default_session = mk (
-          concatStringsSep " " [
-            (getExe pkgs.greetd.tuigreet)
-            "--cmd"
-            (getExe config.programs.hyprland.package)
-            "--asterisks"
-            "--remember"
-            "--remember-session"
-            "--time"
-            "--theme"
-            (concatStringsSep ";" [
-              "border=magenta"
-              "text=cyan"
-              "prompt=green"
-              "time=red"
-              "action=blue"
-              "button=yellow"
-              "container=black"
-              "input=red"
-            ])
-          ]
-        );
+    settings = {
+      initial_session = mk (getExe config.programs.hyprland.package);
+    }
+    // {
+      default_session = mk (
+        concatStringsSep " " [
+          (getExe pkgs.tuigreet)
+          "--cmd"
+          (getExe config.programs.hyprland.package)
+          "--asterisks"
+          "--remember"
+          "--remember-session"
+          "--time"
+          "--theme"
+          (concatStringsSep ";" [
+            "border=magenta"
+            "text=cyan"
+            "prompt=green"
+            "time=red"
+            "action=blue"
+            "button=yellow"
+            "container=black"
+            "input=red"
+          ])
+        ]
+      );
 
-        steam_session = mk "steam-gamescope > /dev/null 2>&1";
-      };
+      steam_session = mk "steam-gamescope > /dev/null 2>&1";
+    };
   };
 }

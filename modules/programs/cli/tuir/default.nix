5979428b3e7acd6bcdf84b5edd43c595124d9bb7
toneus <toneus@blackmamba.org>
2025-09-08 11:54:07 +0900

    tuir: init and upd

---
+++ modules/programs/cli/tuir/default.nix
@@ -0,0 +1,36 @@
+{
+  pkgs,
+  lib,
+  ...
+}:
+
+with lib;
+# INFO: terminal client for reddit
+
+mkIf (mac "pcRyazenka" || mac "thinkpadT14") {
+  environment.systemPackages = [ pkgs.tuir ];
+
+  hm.xdg.configFile = {
+    "tuir/tuir.cfg".text = lib.generators.toINI { } {
+      tuir = {
+        flash = "False";
+        clipboard_cmd = "wl-copy";
+      };
+    };
+  };
+
+  nixpkgs.overlays = [
+    (final: prev: {
+      tuir = prev.tuir.overrideAttrs (old: {
+        version = "1.32.0";
+
+        src = final.fetchFromGitLab {
+          owner = "Chocimier";
+          repo = "tuir";
+          rev = "v${version}";
+          hash = "sha256-lUK6gXwvVjiYrJXMSFlzp07Yt+nSkU933J4vBJWOLlg=";
+        };
+      });
+    })
+  ];
+}

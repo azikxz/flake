cb16b0353459b260f39f624869282dd0d26a84ed
toneus <toneus@blackmamba.org>
2025-09-14 21:05:17 +0900

    gnome-secrets: init

---
+++ modules/programs/gui/secrets/default.nix
@@ -0,0 +1,30 @@
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+
+with lib;
+let
+  db = "${config.hm.xdg.userDirs.documents}/passwords/db.kdbx";
+in
+# INFO: gnome keepass frontend
+
+mkIf false {
+  environment.systemPackages = [ pkgs.gnome-gnome-secrets ];
+
+  hm.dconf.settings = {
+    "org/gnome/World/Secrets" = {
+      generator-length = 32;
+      generator-use-numbers = true;
+      generator-use-symbols = true;
+
+      save-automatically = true;
+      last-opened-database = "file://${db}";
+      last-used-key-provider = ''{ "file://${db}": "{ }" }'';
+
+      sort-order = "a-z";
+    };
+  };
+}

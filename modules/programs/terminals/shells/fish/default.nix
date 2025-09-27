5be2d728db41f9ba21e4ce59c1f6a208d71b1754
nixzoid <nixzoid@jetpure.org>
2025-09-28 00:18:39 +0900

    fish: pretty

--- modules/programs/terminals/shells/fish/default.nix
+++ modules/programs/terminals/shells/fish/default.nix
@@ -11,51 +11,61 @@ mkIf (mac "pcRyazenka" || mac "thinkpadT14") {
 
   programs.fish.enable = true;
 
-  environment.systemPackages = with pkgs; [ grc ];
+  environment.systemPackages = [ pkgs.grc ];
 
-  hm.programs = {
-    fish = {
-      enable = true;
+  hm.programs.fish = {
+    enable = true;
 
-      preferAbbrs = true;
-      shellAbbrs = {
-        gai = "git add --intent-to-add";
-        gus = "git update-index --skip-worktree --assume-unchanged";
-      };
-
-      plugins = import ./plugins.nix {
-        inherit
-          pkgs
-          ;
-      };
-
-      interactiveShellInit =
-        let
-          d = "bind -M default";
-          v = "bind -M visual";
-        in
-        ''
-          fish_vi_key_bindings
-
-          # default
-          ${d} d delete-char
-          ${d} x cancel
-
-          # visual
-          ${v} x down-line
-        ''
-        + import ./colors.nix;
-
-      shellInitLast =
-        # fish
-        ''
-          set fish_cursor_default      block
-          set fish_cursor_insert       line
-          set fish_cursor_replace_one  underscore
-          set fish_cursor_replace      underscore
-          set fish_cursor_visual       block
-          set fish_cursor_external     line
-        '';
+    preferAbbrs = true;
+    shellAbbrs = {
+      gai = "git add --intent-to-add";
+      gus = "git update-index --skip-worktree --assume-unchanged";
     };
+
+    plugins =
+      with pkgs.fishPlugins;
+      map
+        (p: {
+          inherit (p)
+            name
+            src
+            ;
+        })
+        [
+          colored-man-pages
+          autopair-fish
+          fzf-fish
+          git-abbr
+          puffer
+          grc
+        ];
+
+    interactiveShellInit =
+      let
+        d = "bind -M default";
+        v = "bind -M visual";
+      in
+      ''
+        fish_vi_key_bindings
+
+        # default
+        ${d} d delete-char
+        ${d} x cancel
+
+        # visual
+        ${v} x down-line
+      ''
+      + import ./colors.nix;
+
+    shellInitLast =
+      # fish
+      ''
+        set fish_cursor_default      block
+        set fish_cursor_insert       line
+        set fish_cursor_replace_one  underscore
+        set fish_cursor_replace      underscore
+        set fish_cursor_visual       block
+        set fish_cursor_external     line
+      '';
   };
 }

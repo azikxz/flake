51c95f0602ffa5591bd04e13dfc25d3e3098ddb5
nixzoid <nixzoid@jetpure.org>
2025-09-27 17:13:23 +0900

    mindustry: add mod manager

--- modules/programs/games/mindustry/default.nix
+++ modules/programs/games/mindustry/default.nix
@@ -5,31 +5,46 @@
 }:
 
 with lib;
-let
-  server = false;
-in
+# INFO: factory maker
 
 (mkIf (mac "pcRyazenka" || mac "thinkpadT14") {
   persist.user.dirs = [ ".local/share/Mindustry" ];
 
-  environment.systemPackages = with pkgs; [ mindustry-wayland ];
-})
-// (optionalAttrs server {
-  environment.systemPackages = with pkgs; [ mindustry-server ];
+  environment.systemPackages = [ pkgs.mindustry-wayland ];
 
-  networking.firewall =
-    pipe
+  hm.xdg.dataFile =
+    (
+      mods:
+      listToAttrs (
+        map (
+          mod:
+          let
+            modName = removeSuffix ".jar" (baseNameOf mod.url) + ".zip";
+          in
+          {
+            name = "Mindustry/mods/${modName}";
+            value.source = pkgs.runCommand modName { } ''
+              cp ${
+                pkgs.fetchurl {
+                  inherit (mod)
+                    url
+                    sha256
+                    ;
+                }
+              } $out
+            '';
+          }
+        ) mods
+      )
+    )
+      # {
+      #   url = "...";
+      #   sha256 = "..."; # nix store prefetch-file url
+      # }
       [
-        "TCP"
-        "UDP"
-      ]
-      [
-        (builtins.map (
-          protocol:
-          nameValuePair "allowed${protocol}Ports" [
-            cfg.port
-          ]
-        ))
-        builtins.listToAttrs
+        {
+          url = "https://github.com/Yuria-Shikibe/NewHorizonMod/releases/download/v2.0-pre-beta/NewHorizonMod.2.0-pre-beta-1.jar";
+          sha256 = "sha256-4dBpPr46Q5CluitqHIV70LjiDU8DpKTM580eb+YBOJk=";
+        }
       ];
 })

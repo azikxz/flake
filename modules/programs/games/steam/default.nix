c42409c33f774a1ed12b216450cac0bc949fff34
nixzoid <nixzoid@jetpure.org>
2025-09-30 10:11:59 +0900

    steam: fix proton-ge

--- modules/programs/games/steam/default.nix
+++ modules/programs/games/steam/default.nix
@@ -71,7 +71,7 @@ mkIf (mac "pcRyazenka" || mac "thinkpadT14") {
 
   nixpkgs.overlays = [
     (final: prev: {
-      proton-ge-bin = prev.pkgs.proton-ge-bin.override {
+      proton-ge-bin = prev.proton-ge-bin.override {
         steamDisplayName = "Proton-GE";
       };
     })

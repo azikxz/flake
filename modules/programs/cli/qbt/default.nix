08da99813375ccaa146337c16ba4a2ef394731dc
nixzoid <nixzoid@jetpure.org>
2025-09-30 22:14:28 +0900

    qbt: init

---
+++ modules/programs/cli/qbt/default.nix
@@ -0,0 +1,23 @@
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+
+with lib;
+# INFO: qbittorrent cli for service
+# qbt
+
+mkIf config.services.qbittorrent.enable {
+  environment.systemPackages = [ pkgs.qbittorrent-cli ];
+
+  home.file = {
+    ".qbt/settings.json".text = {
+      Url = "http://localhost:8080";
+
+      Username = "pirate";
+      Password = "spRAI+w8UcTTURyl7jGwkfT3caXlxQNozd8yoK25HAo=";
+    };
+  };
+}

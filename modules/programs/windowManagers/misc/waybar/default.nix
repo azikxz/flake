9ac400d1749f9054eebd535ed719b9d3012b14ad
nixzoid <nixzoid@jetpure.org>
2025-10-05 22:37:59 +0900

    waybar: only on hyprland

--- modules/programs/windowManagers/misc/waybar/default.nix
+++ modules/programs/windowManagers/misc/waybar/default.nix
@@ -12,7 +12,10 @@ with lib;
 mkIf (mac "thinkpadT14" || mac "pcRyazenka") {
   hm.programs.waybar = {
     enable = true;
-    systemd.enable = true;
+    systemd = {
+      enable = true;
+      target = "hyprland-session.target";
+    };
 
     style = import ./style.nix {
       inherit

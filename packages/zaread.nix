aa41dc932603209e9b27ae00d3519cc9edb3fc22
nixzoid <nixzoid@jetpure.org>
2025-10-12 23:50:08 +0900

    zaread: init

---
+++ packages/zaread.nix
@@ -0,0 +1,54 @@
+{
+  pkgs,
+  lib,
+}:
+
+pkgs.stdenv.mkDerivation (final: {
+  pname = "zaread";
+  version = "git";
+
+  src = pkgs.fetchurl {
+    url = "https://raw.githubusercontent.com/paoloap/zaread/refs/heads/master/zaread";
+    sha256 = "sha256-BvGbEvMKDx3vnuUHKCzF2/BGOVB/8Cl1P30BO24UT1I=";
+  };
+
+  nativeBuildInputs = [
+    pkgs.autoPatchelfHook
+    pkgs.makeWrapper
+  ];
+
+  dontUnpack = true;
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/bin
+    cp $src $out/bin/zaread
+    chmod +x $out/bin/zaread
+
+    wrapProgram $out/bin/zaread \
+      --prefix PATH ':' \
+        "${
+          lib.makeBinPath (
+            with pkgs;
+            [
+              libreoffice-fresh
+              zathura
+              md2pdf
+              file
+            ]
+          )
+        }"
+
+    runHook postInstall
+  '';
+
+  meta = {
+    description = "A (very) lightweight MS Office file reader via zathura";
+    homepage = "https://github.com/paoloap/zaread";
+    license = lib.licenses.gpl3;
+    platforms = [ "x86_64-linux" ];
+    maintainers = with lib.maintainers; [ azikx ];
+    mainProgram = "zaread";
+  };
+})

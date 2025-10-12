79679f734eb5a86ce8947c7475f2090747cde747
nixzoid <nixzoid@jetpure.org>
2025-10-12 22:45:29 +0900

    torrserver: update 136

--- packages/torrserver.nix
+++ packages/torrserver.nix
@@ -5,21 +5,25 @@
 
 pkgs.stdenv.mkDerivation (final: {
   pname = "torrserver";
-  version = "134";
+  version = "136";
 
   src = pkgs.fetchurl {
     url = "https://github.com/YouROK/TorrServer/releases/download/MatriX.${final.version}/TorrServer-linux-amd64";
-    # hash = "sha256-3HnKxYWCVgexYMdrgbgXVzaC+PjVJJJNs4yQ6oFo8UU="; # 136
-    sha256 = "sha256-WDygG9aGnD20nGxtG0t+T2KEwbJ+fZ0uRaCndirrsXI=";
+    sha256 = "sha256-3HnKxYWCVgexYMdrgbgXVzaC+PjVJJJNs4yQ6oFo8UU="; # 136
+    # sha256 = "sha256-WDygG9aGnD20nGxtG0t+T2KEwbJ+fZ0uRaCndirrsXI="; # 134
   };
 
   nativeBuildInputs = [ pkgs.autoPatchelfHook ];
   dontUnpack = true;
 
   installPhase = ''
+    runHook preInstall
+
     mkdir -p $out/bin
     cp $src $out/bin/torrserver
     chmod +x $out/bin/torrserver
+
+    runHook postInstall
   '';
 
   meta = {

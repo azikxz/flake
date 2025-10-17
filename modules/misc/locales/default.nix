3492b56e6ee2ca0f974a2e1245ee4bca3dadb2b5
nixzoid <nixzoid@jetpure.org>
2025-10-17 20:46:53 +0900

    time: set time zone

--- modules/misc/locales/default.nix
+++ modules/misc/locales/default.nix
@@ -1,5 +1,6 @@
 {
   lib,
+  options,
   ...
 }:
 
@@ -10,8 +11,26 @@ with lib;
 
   services = {
     chrony.enable = true;
-    automatic-timezoned.enable = true;
+    # automatic-timezoned.enable = true;
   };
+
+  time.timeZone = "Asia/Chita";
+
+  networking.timeServers =
+    let
+      inherit (options.networking.timeServers)
+        default
+        ;
+    in
+    (
+      default
+      ++ [
+        "0.ru.pool.ntp.org"
+        "1.ru.pool.ntp.org"
+        "2.ru.pool.ntp.org"
+        "3.ru.pool.ntp.org"
+      ]
+    );
 
   i18n.defaultLocale = "en_US.UTF-8";
 }

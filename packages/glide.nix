310f02bd7bf15a45e8db39eac0744b1d37a81fa9
nixzoid <nixzoid@jetpure.org>
2025-10-24 08:23:36 +0900

    glide: update

--- packages/glide.nix
+++ packages/glide.nix
@@ -4,15 +4,18 @@
 }:
 
 # WARN: broken, im lazy to fixit
-# auto-patchelf failed to find all the required dependencies.
+# another error:
+#
+# ❯ ./result/bin/glide
+# fish: Job 1, './result/bin/glide' terminated by signal SIGSEGV (Address boundary error)
 
 pkgs.stdenv.mkDerivation (final: {
   pname = "glide-browser";
-  version = "0.1.51a";
+  version = "0.1.52a";
 
   src = pkgs.fetchurl {
     url = "https://github.com/glide-browser/glide/releases/download/${final.version}/glide.linux-x86_64.tar.xz";
-    sha256 = "sha256-eQ8itn8XvFNuLypKruTLpWyzDr899lLTMBU3yN+yGeU=";
+    sha256 = "sha256-v3bJLQ+3A4DTBIafQAMHyPdkL+71cpnQlabJYAskUgI=";
   };
 
   nativeBuildInputs = [
@@ -21,10 +24,10 @@ pkgs.stdenv.mkDerivation (final: {
   ];
 
   buildInputs = [
-    pkgs.stdenv.cc.cc.lib
-    pkgs.gcc.libc
-    pkgs.glibc
+    pkgs.alsa-lib
+    pkgs.gtk3
     pkgs.libgcc
+    pkgs.libx11
   ];
 
   dontBuild = true;

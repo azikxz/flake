8bb7eb6fad5c8ceac7ea6d571ccf873eea27f95e
toneus <toneus@blackmamba.org>
2025-10-25 21:20:04 +0900

    thinkpadT14: secrets added

--- machines/thinkpadT14/default.nix
+++ machines/thinkpadT14/default.nix
@@ -3,12 +3,24 @@
   ...
 }:
 
+with lib;
+
 {
-  imports = lib.umport {
+  imports = umport {
     path = ./.;
 
     exclude = [
       ./default.nix
     ];
   };
+
+  sops.secrets = mkSecrets.sops [
+    "syncthing/cert"
+    "syncthing/key"
+  ] ./sopsnix.yaml;
+
+  hm.services.syncthing.settings.devices = listToAttrs [
+    (sync.mkDevice "nothing2a" "U2SSOJT-I43YCNB-HAU4LBI-6UWBLRR-AUTELXZ-PZSCFYJ-J3LKH5H-ROFZBQS")
+    (sync.mkDevice "jetpure" "DW4SRMG-3CK3ZRU-4Q2WQC3-O5FUM77-6BKCEDD-BFMDFOL-PCWOHAX-DUTMHQS")
+  ];
 }
--- machines/thinkpadT14/disko.nix
+++ machines/thinkpadT14/disko.nix
@@ -51,3 +51,24 @@ in
     };
   };
 }
+
+//
+
+  (
+    let
+      mk = name: "/dev/disk/by-uuid/" + name;
+
+      ext4 = {
+        fsType = "ext4";
+        options = [ "x-gvfs-show" ];
+      };
+    in
+
+    {
+      fileSystems = {
+        "/media/disks/mark2" = ext4 // {
+          device = mk "add0974f-e762-457c-96fb-2050609a0288";
+        };
+      };
+    }
+  )

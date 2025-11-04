fa5b2c314de1f12c91600e70b39cc24773b44c10
azixz <xfalwa@gmail.com>
2025-11-05 00:05:56 +0900

    nixhelper: fix diff, so stupid

--- modules/programs/cli/nixHelper/default.nix
+++ modules/programs/cli/nixHelper/default.nix
@@ -8,8 +8,7 @@ with lib;
 
 mkIf (mac "pcRyazenka" || mac "thinkpadT14") {
   environment.shellAliases = {
-    goToFlake = "cd ${paths.flakeDir}";
-    rebuild = "sudo nixos-rebuild --sudo --flake ${paths.flakeDir}#${machine} switch";
+    rebuild = "nh os switch --diff always -H ${machine}";
     nixRepl = "nixos-rebuild repl --flake ${paths.flakeDir}#${machine}";
     nixPre = "nix store prefetch-file";
   };

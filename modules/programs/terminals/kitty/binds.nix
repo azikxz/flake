576cf44159c7fca0a90e1b4f9686d8767ac00d6a
azixz <xfalwa@gmail.com>
2025-11-16 17:34:36 +0900

    kitty: add zoom binds

--- modules/programs/terminals/kitty/binds.nix
+++ modules/programs/terminals/kitty/binds.nix
@@ -23,6 +23,11 @@ in
   "${kitty_mod}+]" = "set_background_opacity +0.1";
   "${kitty_mod}+[" = "set_background_opacity -0.1";
 
+  "${kitty_mod}+equal" = "change_font_size all +2.0";
+  "${kitty_mod}+plus" = "change_font_size all +2.0";
+  "${kitty_mod}+minus" = "change_font_size all -2.0";
+  "${kitty_mod}+backspace" = "change_font_size all 0";
+
   "page_up" = "scroll_page_up";
   "page_down" = "scroll_page_down";
   "ctrl+page_up" = "scroll_line_up";

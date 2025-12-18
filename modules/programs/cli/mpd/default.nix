c85d51f5e39e86763e5036e367bcd34fca80af73
azixz <xfalwa@gmail.com>
2025-12-18 18:03:30 +0900

    mpd: bit rework

--- modules/programs/cli/mpd/default.nix
+++ modules/programs/cli/mpd/default.nix
@@ -1,51 +1,36 @@
 {
   lib,
-  config,
   ...
 }:
 
 with lib;
-let
-  inherit (config.hm.services.mpd)
-    dataDir
-    ;
-
-  socket = "${dataDir}/mpd_socket";
-  fifo = "${dataDir}/fifo_socket";
-in
 # INFO:
 # idk lazy to fix...zzZ
 
 mkIf (mac "pcRyazenka" || mac "thinkpadT14") {
   hm = {
-    programs.rmpc = {
-      enable = true;
-      config = import ./settings.nix {
-        inherit
-          socket
-          fifo
-          ;
+    services = {
+      mpd = {
+        enable = true;
+
+        extraConfig = # conf
+          ''
+            audio_output {
+              type "pipewire"
+              name "PipeWire Output"
+            }
+          '';
+
+        network = {
+          startWhenNeeded = true;
+        };
       };
-    };
 
-    services.mpd = {
-      enable = true;
-      extraConfig = # conf
-        ''
-          audio_output {
-            type "pipewire"
-            name "PipeWire Output"
-          }
+      mpd-mpris = {
+        enable = true;
 
-          audio_output {
-             type   "fifo"
-             name   "Fifo Output"
-             path   "${fifo}"
-             format "44100:16:2"
-          }
-
-          bind_to_address "${socket}"
-        '';
+        mpd.useLocal = true;
+      };
     };
   };
 }

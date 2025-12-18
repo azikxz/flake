ef72930795761f7a71a13be7678164d3498a2a9b
azixz <xfalwa@gmail.com>
2025-12-18 23:15:46 +0900

    network: rm unusable

--- modules/hardware/network/default.nix
+++ modules/hardware/network/default.nix
@@ -5,9 +5,6 @@
 }:
 
 with lib;
-let
-  dnsEnable = false;
-in
 # INFO:
 # connect via nmtui
 # also it contains dns resolving
@@ -36,9 +33,11 @@ in
       };
     };
 
-    nameservers = mkIf (dnsEnable != true) [
+    nameservers = [
       "1.1.1.1"
       "8.8.8.8"
+      "84.21.189.133"
+      "83.220.169.155"
     ];
 
     extraHosts = readFile (inputs.dns-malw-link.outPath + "/hosts");
@@ -49,26 +48,3 @@ in
     indicator = true;
   };
 }
-// (optionalAttrs dnsEnable {
-  networking.nameservers = [
-    "1.1.1.1#cloudflare-dns.com"
-    "8.8.8.8#dns.google"
-
-    "83.220.169.155#dns.comss.one"
-
-    # hetzner
-    "185.12.64.1"
-    "185.12.64.2"
-    "2a01:4ff:ff00::add:1"
-    "2a01:4ff:ff00::add:2"
-  ];
-
-  services.resolved = {
-    enable = false;
-
-    dnsovertls = "true";
-    llmnr = "true";
-
-    fallbackDns = [ "8.8.8.8#dns.google" ];
-  };
-})

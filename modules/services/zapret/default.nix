56a82fee0f54ff9634e9e2873ed5379c056e0e19
azixz <xfalwa@gmail.com>
2025-12-20 15:59:56 +0900

    zapret: finally

--- modules/services/zapret/default.nix
+++ modules/services/zapret/default.nix
@@ -5,7 +5,7 @@
 
 with lib;
 let
-  revision = "zapret"; # zapret / zapret-alt
+  revision = "zapret-alt"; # zapret / zapret-alt
 in
 
 mkIf (mac "pcRyazenka" || mac "thinkpadT14") (
@@ -37,7 +37,7 @@ mkIf (mac "pcRyazenka" || mac "thinkpadT14") (
 
         zapret-discord-youtube = {
           enable = true;
-          config = "general (SIMPLE FAKE)";
+          config = "general(ALT7)";
         };
       };
     }
@@ -76,7 +76,7 @@ mkIf (mac "pcRyazenka" || mac "thinkpadT14") (
 
           sf_presets = {
             enable = true;
-            preset = "renixos";
+            preset = "general_alt6";
           };
         };
       };

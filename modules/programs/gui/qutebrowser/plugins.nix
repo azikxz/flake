9632315497a0685b191a9be9d6344af85c58c24b
azixz <xfalwa@gmail.com>
2026-01-07 16:36:19 +0900

    qutebrowser: remove vot

--- modules/programs/gui/qutebrowser/plugins.nix
+++ modules/programs/gui/qutebrowser/plugins.nix
@@ -17,11 +17,6 @@ with pkgs;
   })
 
   (fetchurl {
-    url = "https://gist.githubusercontent.com/azikxz/482d955868b9fb625ad4d9339a1f24ed/raw/fc6a49e627ddbec1266b686c28dbd0c495275d2d/vot.user.js";
-    sha256 = "sha256-TNG8qiEVXpFRC9ygwX+rGiqx0Szi8XF0NROpx8F8Cuk=";
-  })
-
-  (fetchurl {
     url = "https://gist.githubusercontent.com/ilyhalight/6eb5bb4dffc7ca9e3c57d6933e2452f3/raw/7ab38af2228d0bed13912e503bc8a9ee4b11828d/gm-addstyle-polyfill.js";
     sha256 = "sha256-C4NkK3NGDV0XehzoStm0lTUVT6XUMy4app+vAFpxY0E=";
   })
